f17b92203f23e9b4e5c15065b754085cfa5f18e2
Donathan Queiros <66445710+donathanqueiros@users.noreply.github.com>
2022-05-12 13:51:54 -0300

    Update useHorizontalScroll.ts

--- src/hooks/useHorizontalScroll.ts
+++ src/hooks/useHorizontalScroll.ts
@@ -11,7 +11,7 @@ export function useHorizontalScroll() {
         if (e.deltaY === 0) return;
         e.preventDefault();
         el.scrollTo({
-          left: el.scrollLeft + e.deltaY,
+          left: el.scrollLeft + e.deltaY * 3,
           behavior: "smooth",
         });
       };
